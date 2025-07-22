d7b7da1d7db2837bfa872928680415e0de8facd4
Samman Qaiser <sammansheikh6@gmail.com>
2025-07-22 22:28:29 +0500

    'updated'

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -6,12 +6,8 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    const conn = await mongoose.connect(process.env.URI);
+    console.log(`MongoDB Connected`);
   } catch (error) {
     console.error(`Error connecting to MongoDB: ${error.message}`);
     process.exit(1); // Exit process with failure
